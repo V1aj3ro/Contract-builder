44e1fdc5e27e91418fad6254ea84d9b0aba9a8fe
Viajero <viajero95@mail.ru>
2026-04-06 08:46:03 +0700

    add Pico CSS styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <nav>
-    <RouterLink to="/">Договоры</RouterLink>
-    |
-    <RouterLink to="/customers">Заказчики</RouterLink>
-    |
-    <RouterLink to="/disciplines">Дисциплины</RouterLink>
-    |
-    <RouterLink to="/contracts/new">Новый договор</RouterLink>
-    | 
-    <RouterLink to="/contractors">Исполнители</RouterLink>
-  </nav>
-  <RouterView />
+  <header class="container">
+    <nav>
+      <ul>
+        <li><strong>Contract Builder</strong></li>
+      </ul>
+      <ul>
+        <li><RouterLink to="/">Договоры</RouterLink></li>
+        <li><RouterLink to="/contractors">Исполнители</RouterLink></li>
+        <li><RouterLink to="/customers">Заказчики</RouterLink></li>
+        <li><RouterLink to="/disciplines">Дисциплины</RouterLink></li>
+        <li><RouterLink to="/contracts/new"><strong>+ Новый договор</strong></RouterLink></li>
+      </ul>
+    </nav>
+  </header>
+  <main class="container">
+    <RouterView />
+  </main>
 </template>
